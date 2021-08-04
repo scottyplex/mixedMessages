b186b7dcc0b5a7bf8bbaa540fa3fee3280c2e630
scottyplex <scottyplexgit@gmail.com>
2021-08-04 03:47:28 -0500

    create mealMaker script

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,14 @@
 function randomMessage() {
-    var rnd = Math.floor(Math.random() * 1000000000);
-    document.getElementById("message").innerHTML = rnd;
+    protein = ['Chicken ', 'Steak ', 'Shrimp ', 'Ham ', 'Salmon ', 'Ribs ', 'Wings ', 'Burger '];
+    starch = ['Rice ', 'Couscous ', 'Mashed Potatoes ', 'French Fries ', ''];
+    veggie = ['Corn on the Cobb ', 'Green Beans ', 'Carrots ', 'Peas ', 'Broccoli ', 'Brussel Sprouts ', 'Sweet Potatoes', 'Asparagus '];
+    desert = ['Cupcakes', 'Jell-O', 'Carrot Cake', 'Apple Pie', 'Ice Cream', 'Brownies', 'Chocolate Chip Cookies', 'Chocolate Cake'];
+
+    const randomize = (array) => {
+    return array[Math.floor(Math.random()*array.length)];
+};
+
+    let mealMaker = randomize(protein) + ", " + randomize(starch) + ", " +  randomize(veggie) + "and " + randomize(desert);
+
+    document.getElementById("message").innerHTML = mealMaker;
 }
