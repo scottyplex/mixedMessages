6abccb7f1342653b1054838aac55a645340d10dd
scottyplex <scottyplexgit@gmail.com>
2021-08-04 04:05:20 -0500

    delete blank index in starch array

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,6 @@
 function randomMessage() {
     protein = ['Chicken ', 'Steak ', 'Shrimp ', 'Ham ', 'Salmon ', 'Ribs ', 'Wings ', 'Burger '];
-    starch = ['Rice ', 'Couscous ', 'Mashed Potatoes ', 'French Fries ', ''];
+    starch = ['Rice ', 'Couscous ', 'Mashed Potatoes ', 'French Fries ',];
     veggie = ['Corn on the Cobb ', 'Green Beans ', 'Carrots ', 'Peas ', 'Broccoli ', 'Brussel Sprouts ', 'Sweet Potatoes', 'Asparagus '];
     desert = ['Cupcakes', 'Jell-O', 'Carrot Cake', 'Apple Pie', 'Ice Cream', 'Brownies', 'Chocolate Chip Cookies', 'Chocolate Cake'];
 
